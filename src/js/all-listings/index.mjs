5c46c74cc4cf89418de000de96007659806a871f
MariuszRozycki <mariansc83@icloud.com>
2024-01-23 07:27:00 +0100

    Deploy test

--- src/js/all-listings/index.mjs
+++ src/js/all-listings/index.mjs
@@ -5,18 +5,28 @@ import { renderListings } from "./renderListings.mjs";
 import { showMoreLessFunction } from "../utils/showMoreLessFunction.mjs";
 
 export const allListings = async () => {
+  const repoName = "/auction-gavel/";
   const path = location.pathname;
+  const isRootPath = path === "/" || path.startsWith(repoName);
+
   const showMoreBtn = document.querySelector("#show-more-btn");
   const showLessBtn = document.querySelector("#show-less-btn");
+
   const limitNr = 10;
   const offsetNr = 0;
   const maxLimit = 100;
   const URL_limited_to_10 = `${URL_allListings}?limit=${limitNr}&offset=${offsetNr}`;
-  if (path === "/") {
-    const jsonAllListings = await getListings(URL_allListings);
-    const jsonLimitedTo_10 = await getListings(URL_limited_to_10);
-    renderListings(jsonLimitedTo_10, path, offsetNr + 1);
-    showMoreLessFunction(showMoreBtn, showLessBtn, limitNr, offsetNr, maxLimit, URL_limited_to_10, path);
-    search(jsonAllListings, path, offsetNr, showLessBtn, showMoreBtn, jsonLimitedTo_10);
+
+  if (isRootPath) {
+    try {
+      const jsonAllListings = await getListings(URL_allListings);
+      const jsonLimitedTo_10 = await getListings(URL_limited_to_10);
+
+      renderListings(jsonLimitedTo_10, path, offsetNr + 1);
+      showMoreLessFunction(showMoreBtn, showLessBtn, limitNr, offsetNr, maxLimit, URL_limited_to_10, path);
+      search(jsonAllListings, path, offsetNr, showLessBtn, showMoreBtn, jsonLimitedTo_10);
+    } catch (error) {
+      console.error("Error loading all listings:", error);
+    }
   }
 };
