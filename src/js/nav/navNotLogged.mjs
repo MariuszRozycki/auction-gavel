d45299397031a47b618ea6577abb12288d2a3310
MariuszRozycki <mariansc83@icloud.com>
2024-01-25 07:08:50 +0100

    Add correct 'aria-current' display on correct nav el

--- src/js/nav/navNotLogged.mjs
+++ src/js/nav/navNotLogged.mjs
@@ -1,65 +1,35 @@
 import { createElement } from "../utils/createElement.mjs";
 
 export const navNotLogged = () => {
-  console.log("nie zalogowany");
+  // console.log("nie zalogowany");
+  const path = location.pathname;
   const navItems = [
-    { text: "Home", href: "./index.html" },
-    { text: "About Us", href: "./about.html" },
-    { text: "Contact", href: "./../../../pages/contact/" },
-    { text: "Log in", href: "./pages/log-in/" },
+    { text: "Home", href: "/" },
+    { text: "About Us", href: "/pages/about/" },
+    { text: "Contact", href: "/pages/contact/" },
+    { text: "Log in", href: "/pages/log-in/" },
   ];
 
   const navContainer = document.getElementById("nav-container");
 
   navItems.forEach((item) => {
-    // Sprawdź, czy aktualny adres URL pasuje do href elementu nawigacji
-    const isCurrentPage = location.pathname.endsWith(item.href) || (location.pathname === "/" && item.href === "/");
-    console.log("isCurrentPage", isCurrentPage);
-
     const listItem = createElement("li", "nav-item");
     const link = createElement("a", "nav-link", item.text, {
       href: item.href,
-      // Ustaw 'aria-current' na 'page' tylko jeśli to aktualna strona
-      "aria-current": isCurrentPage ? "page" : undefined,
     });
+
+    if (path === item.href) {
+      link.setAttribute("aria-current", "page");
+    }
 
     listItem.appendChild(link);
     navContainer.appendChild(listItem);
 
-    // Dodajemy również podział między elementami nawigacji
-    if (item.text !== "Log in") {
+    console.log(link);
+
+    if (item.text === "Home") {
       const divider = createElement("hr", "divider");
       navContainer.appendChild(divider);
     }
   });
 };
-// import { createElement } from "../utils/createElement.mjs";
-
-// export const navNotLogged = () => {
-//   const navContainer = document.querySelector("#nav-container");
-//   const navItems = [
-//     { text: "Home", href: "./index.html", current: true },
-//     { text: "About Us", href: "./about.html" },
-//     { text: "Contact", href: "./contact.html" },
-//     { text: "Log in", href: "./pages/log-in/" },
-//   ];
-
-//   navItems.forEach((item) => {
-//     console.log("item", item);
-//     const listItem = createElement("li", "nav-item");
-//     const link = createElement("a", "nav-link", item.text, {
-//       href: item.href,
-//       "aria-current": item.current ? "page" : undefined,
-//     });
-
-//     listItem.appendChild(link);
-//     navContainer.appendChild(listItem);
-
-//     if (item.text !== "Log in") {
-//       const divider = createElement("hr", "divider");
-//       navContainer.appendChild(divider);
-//     }
-//   });
-
-//   console.log("User is not logged.");
-// };
