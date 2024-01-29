93eadc29fcbccbb210618099e7684b9554c3a422
MariuszRozycki <mariansc83@icloud.com>
2024-01-29 06:21:23 +0100

    Add logInOnMainBtn call to file

--- src/js/index.mjs
+++ src/js/index.mjs
@@ -2,8 +2,10 @@ import { registerPath } from "./userData.mjs/index.mjs";
 import { loginPath } from "./userData.mjs/index.mjs";
 import { allListings } from "./all-listings/index.mjs";
 import { navDisplay } from "./nav/index.mjs";
+import { logInOnMainBtn } from "./utils/loginOnMainBtn.mjs";
 
 registerPath();
 loginPath();
 allListings();
 navDisplay();
+logInOnMainBtn();
