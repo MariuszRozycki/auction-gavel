888bf15b3655a7bbdfd3b14f9928b4d652cd85df
MariuszRozycki <mariansc83@icloud.com>
2024-01-29 06:00:55 +0100

    Add new features to src/js/nav/navLogged.mjs

--- src/js/nav/navLogged.mjs
+++ src/js/nav/navLogged.mjs
@@ -1,4 +1,5 @@
 import { createElement } from "../utils/createElement.mjs";
+import { logOut } from "../auth/logOut.mjs";
 
 export const navLogged = (navContainer) => {
   console.log("Pomyslnie zalogowany!");
@@ -9,18 +10,25 @@ export const navLogged = (navContainer) => {
     { text: "About Us", href: "/pages/about/" },
     { text: "Contact", href: "/pages/contact/" },
     { text: "Update avatar", href: "/pages/update-avatar/" },
-    { text: "Log out", href: "/pages/log-out/" },
+    { text: "Log out", href: "#" },
   ];
 
   navContainer.innerHTML = "";
 
   navItems.forEach((item) => {
+    const { text, href } = item;
     const listItem = createElement("li", "nav-item");
-    const link = createElement("a", "nav-link", item.text, {
-      href: item.href,
-    });
+    const linkAttributes = {
+      href: href,
+    };
 
-    if (path === item.href) {
+    if (text === "Log out") {
+      linkAttributes.id = "log-out-btn";
+    }
+
+    const link = createElement("a", "nav-link", text, linkAttributes);
+
+    if (path === href) {
       link.setAttribute("aria-current", "page");
     }
 
@@ -29,9 +37,12 @@ export const navLogged = (navContainer) => {
 
     console.log(link);
 
-    if (item.text === "Home") {
+    if (text === "Home") {
       const divider = createElement("hr", "divider");
       navContainer.appendChild(divider);
     }
   });
+
+  const logOutButton = document.querySelector("#log-out-btn");
+  logOut(logOutButton);
 };
