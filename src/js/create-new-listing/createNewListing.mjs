4b362aa7f0e30472a91645c1ec3035d2decaa2c5
MariuszRozycki <mariansc83@icloud.com>
2024-02-05 13:08:12 +0100

    Add error handling in function createNewListening

--- src/js/create-new-listing/createNewListing.mjs
+++ src/js/create-new-listing/createNewListing.mjs
@@ -1,42 +1,85 @@
 import { URL_base } from "../api/index.mjs";
-import { URL_allListings } from "../api/index.mjs";
-// import { guidGenerator } from "../utils/guidGenerator.mjs";
 import { newListingData } from "./newListingData.mjs";
 import { authWithToken } from "../auth/authWithToken.mjs";
-import { getListings } from "../all-listings/getListings.mjs";
 
 export const createNewListing = () => {
-  // const guid = guidGenerator();
+  const listingCreatedSuccess = document.querySelector("#new-listing-created");
   const URL_newListing = URL_base + `/auction/listings`;
   const newListingForm = document.querySelector("#new-listing-form");
-  console.log(URL_newListing);
   const method = "POST";
-
-  const endsAt = document.querySelector("#new-listing-endsAt");
-  endsAt.value = "2024-02-11T23:59:59.940Z";
 
   newListingForm.addEventListener("submit", async (e) => {
     e.preventDefault();
-    const newListingTitleValue = document.querySelector("#new-listing-title").value;
-    const newListingDescriptionValue = document.querySelector("#new-listing-description").value;
-    const newListingsEndsAtValue = endsAt.value;
-    console.log("newListingsEndsAtValue", newListingsEndsAtValue);
-    const newListingTags = document.querySelector("#new-listing-tags").value;
-    const newListingMedia = document.querySelector("#new-listing-media").value;
+
+    const newListingTitle = document.querySelector("#new-listing-title");
+    const newListingDescription = document.querySelector("#new-listing-description");
+    const newListingsEndsAt = document.querySelector("#new-listing-endsAt");
+    const newListingTags = document.querySelector("#new-listing-tags");
+    const newListingMedia = document.querySelector("#new-listing-media");
+
+    const newListingTitleValue = newListingTitle.value;
+    const newListingDescriptionValue = newListingDescription.value;
+    const newListingsEndsAtValue = newListingsEndsAt.value;
+    const newListingTagsValue = newListingTags.value;
+    const newListingMediaValue = newListingMedia.value;
+
+    const endsAtError = document.querySelector(".new-listing-endsAt-error");
+    const mediaError = document.querySelector(".new-listing-media-error");
 
     const newListingsDataValue = newListingData(
       newListingTitleValue,
       newListingDescriptionValue,
       newListingsEndsAtValue,
-      newListingTags,
-      newListingMedia,
+      newListingTagsValue,
+      newListingMediaValue,
     );
 
     try {
+      let isDateError = false;
+      let isMediaError = false;
+      let listingCreatedFailure = false;
+
       const json = await authWithToken(method, URL_newListing, newListingsDataValue);
-      console.log("json: ", json);
-      const jsonAll = await getListings(URL_allListings);
-      console.log("json in createNewListing", jsonAll);
+      const jsonBadRequest = json.json.status;
+
+      if (jsonBadRequest) {
+        const jsonErrors = json.json.errors;
+
+        for (const error of jsonErrors) {
+          const errorMessage = error.message;
+
+          switch (true) {
+            case errorMessage.includes("date"):
+              isDateError = true;
+              listingCreatedFailure = true;
+              endsAtError.classList.remove("d-none");
+              endsAtError.innerText = `${errorMessage}`;
+              break;
+            case errorMessage.includes("URL"):
+              isMediaError = true;
+              listingCreatedFailure = true;
+              mediaError.classList.remove("d-none");
+              mediaError.innerText = `${errorMessage}`;
+              break;
+            default:
+          }
+
+          if (!isDateError) endsAtError.classList.add("d-none");
+          if (!isMediaError) mediaError.classList.add("d-none");
+          if (!listingCreatedFailure) listingCreatedSuccess.classList.remove("d-none");
+        }
+      } else {
+        newListingTitle.value = "";
+        newListingDescription.value = "";
+        newListingsEndsAt.value = "";
+        newListingTags.value = "";
+        newListingMedia.value = "";
+
+        endsAtError.classList.add("d-none");
+        mediaError.classList.add("d-none");
+
+        listingCreatedSuccess.classList.remove("d-none");
+      }
     } catch (error) {
       console.error();
     }
