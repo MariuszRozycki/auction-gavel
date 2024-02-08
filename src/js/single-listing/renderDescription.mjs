b66a7ab18be3104191ed57b6710c1cfec09ee1d3
MariuszRozycki <mariansc83@icloud.com>
2024-02-08 13:48:53 +0100

    Add divider as a listItem

--- src/js/single-listing/renderDescription.mjs
+++ src/js/single-listing/renderDescription.mjs
@@ -6,17 +6,24 @@ export const renderDescription = (title, description) => {
   const objectDetailsHeader = createElement("h2", "object-details-header", "Object details: ");
   const objectDetailsWrapper = createElement("ul", "object-details-wrapper");
 
-  const listItemTitle = createElement("li", "object-detail-element", title);
+  const listItemTitle = createElement("li", "object-detail-element");
   const spanItemTitle = createElement("span", "fw-bold", "Title: ");
+  listItemTitle.appendChild(spanItemTitle);
+  listItemTitle.append(title);
 
-  const listItemDescription = createElement("li", "object-detail-element", description);
+  const listItemDivider = createElement("li", "divider");
+  const hr = document.createElement("hr");
+  listItemDivider.appendChild(hr);
+
+  const listItemDescription = createElement("li", "object-detail-element");
   const spanItemDescription = createElement("span", "fw-bold", "Description: ");
+  listItemDescription.appendChild(spanItemDescription);
+  listItemDescription.append(description);
 
   listingDescriptionContainer.appendChild(objectDetailsHeader);
   listingDescriptionContainer.appendChild(objectDetailsWrapper);
   objectDetailsWrapper.appendChild(listItemTitle);
-  listItemTitle.prepend(spanItemTitle);
-
+  objectDetailsWrapper.appendChild(listItemDivider);
   objectDetailsWrapper.appendChild(listItemDescription);
-  listItemDescription.prepend(spanItemDescription);
 };
+``;
