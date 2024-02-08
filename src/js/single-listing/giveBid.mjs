03421b2e8443b2c3ecbd4f74948acf5637a1232d
MariuszRozycki <mariansc83@icloud.com>
2024-02-08 14:41:58 +0100

    Working on giveBid if seller and loggedUserName are different

--- src/js/single-listing/giveBid.mjs
+++ src/js/single-listing/giveBid.mjs
@@ -1,12 +1,17 @@
-export const giveBid = (container, data) => {
+import { URL_base } from "../api/index.mjs";
+import { newBidData } from "./newBidData.mjs";
+import { authWithToken } from "../auth/authWithToken.mjs";
+
+export const giveBid = (container, data, id) => {
   container.classList.remove("d-none");
   const dataParsed = JSON.parse(data);
+  console.log("dataParsed: ", dataParsed);
   let { name, credits } = dataParsed;
   let currentlyCredits = credits;
 
   const giveBidForm = document.querySelector("#give-bid-form");
   const inputBid = document.querySelector("#bid");
-  giveBidForm.addEventListener("submit", (e) => {
+  giveBidForm.addEventListener("submit", async (e) => {
     e.preventDefault();
     const inputBidValue = inputBid.value;
 
@@ -17,6 +22,18 @@ export const giveBid = (container, data) => {
 
     console.log("credits", credits);
     console.log("inputBidValue", inputBidValue);
+
+    const method = "POST";
+    const URL_bidsUpdate = `${URL_base}/auction/listings/${id}/bids?_seller=true&_bids=true`;
+    const newBidDataValue = newBidData(credits);
+    console.log("newBidDataValue: ", newBidDataValue);
+
+    try {
+      const json = await authWithToken(method, URL_bidsUpdate, newBidDataValue);
+      console.log(json);
+    } catch (error) {
+      console.error(error);
+    }
 
     // if (inputBidValue > credits) {
     //   console.log(`You don't have enough credits!`);
--- src/js/single-listing/renderSingleListing.mjs
+++ src/js/single-listing/renderSingleListing.mjs
@@ -5,13 +5,15 @@ import { renderCarousel } from "../utils/renderCarousel.mjs";
 import { renderDescription } from "./renderDescription.mjs";
 import { giveBid } from "./giveBid.mjs";
 
-export const renderSingleListing = async (id) => {
+export const renderSingleListing = async (singleListingId) => {
   const singleListingContainer = document.querySelector("#single-listing-container");
   const giveBidContainer = document.querySelector("#give-bid-container");
   singleListingContainer.innerHTML = "";
   const userData = localStorage.getItem("USER_DATA");
+  const userDataParsed = JSON.parse(userData);
+  console.log("userDataParsed:", userDataParsed);
   const titleNotExists = "Title not exists";
-  const URL_singleListing = `${URL_base}/auction/listings/${id}?_seller=true&_bids=true`;
+  const URL_singleListing = `${URL_base}/auction/listings/${singleListingId}?_seller=true&_bids=true`;
 
   try {
     const singleListingData = await getListings(URL_singleListing);
@@ -22,11 +24,13 @@ export const renderSingleListing = async (id) => {
       description,
       endsAt,
       media,
-      seller: { name },
+      seller: { name: sellerName },
       title,
       tags,
       updated,
     } = singleListingData;
+
+    console.log(sellerName);
 
     const titleValue = title || titleNotExists;
 
@@ -34,13 +38,16 @@ export const renderSingleListing = async (id) => {
     createHeader(singleListingContainer);
 
     /* carousel */
-    renderCarousel(id, media, title);
+    renderCarousel(singleListingId, media, title);
 
     /* description */
     renderDescription(title, description);
 
     /* give a bid */
-    if (userData) giveBid(giveBidContainer, userData);
+    const { loggedUserName } = userDataParsed;
+    if (sellerName !== loggedUserName) {
+      giveBid(giveBidContainer, userData, singleListingId);
+    }
   } catch (error) {
     console.error(error);
   }
