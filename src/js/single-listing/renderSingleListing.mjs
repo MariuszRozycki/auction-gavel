7cc288b5a4387148f1ef786b873d0cf3a636207f
MariuszRozycki <mariansc83@icloud.com>
2024-02-08 13:50:28 +0100

    Code refactoring

--- src/js/single-listing/renderSingleListing.mjs
+++ src/js/single-listing/renderSingleListing.mjs
@@ -1,15 +1,16 @@
 import { URL_base } from "../api/index.mjs";
 import { getListings } from "../all-listings/getListings.mjs";
-import { createElement } from "../utils/createElement.mjs";
+import { createHeader } from "./createHeader.mjs";
 import { renderCarousel } from "../utils/renderCarousel.mjs";
 import { renderDescription } from "./renderDescription.mjs";
+import { giveBid } from "./giveBid.mjs";
 
 export const renderSingleListing = async (id) => {
   const singleListingContainer = document.querySelector("#single-listing-container");
+  const giveBidContainer = document.querySelector("#give-bid-container");
   singleListingContainer.innerHTML = "";
-
+  const userData = localStorage.getItem("USER_DATA");
   const titleNotExists = "Title not exists";
-
   const URL_singleListing = `${URL_base}/auction/listings/${id}?_seller=true&_bids=true`;
 
   try {
@@ -29,38 +30,17 @@ export const renderSingleListing = async (id) => {
 
     const titleValue = title || titleNotExists;
 
-    /* header */
-    const singleListingH1 = createElement("h1", "h1 h1-header-focus text-center text-light my-0", "Listing details");
-    // const dataTitle = createElement("span", null, titleValue);
-    const row = createElement("div");
-    const backToAllListingsContainer = createElement(
-      "div",
-      "col-10 col-sm-10 col-md-8 mx-auto my-3 focus-bg d-flex justify-content-center align-items-center rounded",
-      null,
-      { id: "back-to-all-listings-container" },
-    );
-    const goBackLinkBtn = createElement(
-      "a",
-      "btn support-message text-light mx-auto text-center my-0 py-3 w-100 fs-6 d-flex justify-content-center align-items-center",
-      "View all listings",
-      { href: "../../../pages/user-details/" },
-    );
-    const goBackImg = createElement("img", "go-back-arrow", null, {
-      src: "../../../images/pictures/arrow-go-back.svg",
-    });
-
-    singleListingContainer.appendChild(singleListingH1);
-    // singleListingH1.appendChild(dataTitle);
-    singleListingContainer.appendChild(row);
-    row.appendChild(backToAllListingsContainer);
-    backToAllListingsContainer.appendChild(goBackLinkBtn);
-    goBackLinkBtn.prepend(goBackImg);
+    /* heading */
+    createHeader(singleListingContainer);
 
     /* carousel */
     renderCarousel(id, media, title);
 
     /* description */
     renderDescription(title, description);
+
+    /* give a bid */
+    if (userData) giveBid(giveBidContainer, userData);
   } catch (error) {
     console.error(error);
   }
