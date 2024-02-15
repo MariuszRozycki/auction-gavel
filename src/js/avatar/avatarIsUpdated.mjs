1c3a63afbeecd9727909976cd427a696131e37d7
MariuszRozycki <mariansc83@icloud.com>
2024-02-15 21:04:55 +0100

    Add JSDocs for avatarIsUpdated

--- src/js/avatar/avatarIsUpdated.mjs
+++ src/js/avatar/avatarIsUpdated.mjs
@@ -1,16 +1,31 @@
 import { createElement } from "../utils/createElement.mjs";
+import { displayError } from "../utils/displayError.mjs";
+
+/**
+ * Updates user's avatar from localStorage and sets event for avatar update.
+ *
+ * @example
+ * avatarIsUpdated();
+ */
 
 export const avatarIsUpdated = () => {
-  const localStorageData = localStorage.getItem("USER_DATA");
-  const parsedLocal = JSON.parse(localStorageData);
-  const { avatar: newAvatar } = parsedLocal;
+  try {
+    const localStorageData = localStorage.getItem("USER_DATA");
+    if (!localStorageData) throw new Error("No USER_DATA found in localStorage.");
 
-  const newAvatarContainer = document.querySelector(".new-avatar-container");
-  const newAvatarImg = createElement("img", "new-avatar-img", null, { src: newAvatar });
+    const parsedLocal = JSON.parse(localStorageData);
+    const { avatar: newAvatar } = parsedLocal;
 
-  newAvatarContainer.appendChild(newAvatarImg);
+    const newAvatarContainer = document.querySelector(".new-avatar-container");
+    if (!newAvatarContainer) throw new Error(".new-avatar-container not found in the DOM.");
 
-  /* update avatar again button */
+    const newAvatarImg = createElement("img", "new-avatar-img", null, { src: newAvatar });
+    newAvatarContainer.appendChild(newAvatarImg);
+  } catch (error) {
+    displayError();
+  }
+
+  // Update avatar again button
   document.querySelector("#update-again").addEventListener("click", () => {
     window.location.href = "../../../pages/update-avatar/";
   });
