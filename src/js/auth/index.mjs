78b0c6b7220595ae6d2c0a6c9f0f102c876c35ca
MariuszRozycki <mariansc83@icloud.com>
2024-02-15 10:12:35 +0100

    Add JSDocs for src/js/auth/index.mjs function isUserLoggedPath

--- src/js/auth/index.mjs
+++ src/js/auth/index.mjs
@@ -1,5 +1,16 @@
 import { isUserLogged } from "./isUserLogged.mjs";
 
+/**
+ * Checks if the user is on the root path and has user data stored in localStorage.
+ * If both conditions are met, it calls the `isUserLogged` function with the user data.
+ * This function is typically used to verify if a logged-in user is accessing the home page.
+ * @example
+ * ```js
+ * // To check if a logged-in user is on the home page and take appropriate action
+ * isUserLoggedPath();
+ * ```
+ */
+
 export const isUserLoggedPath = () => {
   const userData = localStorage.getItem("USER_DATA");
   const path = location.pathname;
