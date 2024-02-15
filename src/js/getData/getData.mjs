d0b16876ff2463e3aefc5b5c2b79b4c1fc35a167
MariuszRozycki <mariansc83@icloud.com>
2024-02-15 21:39:27 +0100

    Add JSDocs for functions inside directories getData & my-credits

--- src/js/getData/getData.mjs
+++ src/js/getData/getData.mjs
@@ -1,5 +1,20 @@
 import { displayError } from "../utils/displayError.mjs";
 
+/**
+ * Asynchronously fetches data from the specified URL and returns the parsed JSON.
+ *
+ * @param {string} url The URL from which to fetch data.
+ * @returns {Promise<Object>} A promise that resolves with the parsed JSON data from the response.
+ * @throws {Error} Rethrows any error caught during the fetch operation or JSON parsing.
+ *
+ * @example
+ * // Example of using getData to fetch data from an API
+ * const apiUrl = "https://api.example.com/data";
+ * getData(apiUrl)
+ *   .then(data => console.log(data))
+ *   .catch(error => console.error("Failed to fetch data:", error));
+ */
+
 export const getData = async (url) => {
   try {
     const response = await fetch(url);
