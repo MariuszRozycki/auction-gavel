914fdac3c4da309cec6c984212f29a9cfd69aae2
MariuszRozycki <mariansc83@icloud.com>
2024-02-18 12:03:20 +0100

    Remove search bar from nav in all *.html files

--- src/js/nav/index.mjs
+++ src/js/nav/index.mjs
@@ -1,13 +1,10 @@
 import { navNotLogged } from "./navNotLogged.mjs";
 import { navLogged } from "./navLogged.mjs";
-import { navSearchDisplay } from "./navSearchDisplay.mjs";
 
 /**
  * Dynamically updates the navigation bar based on the user's logged-in status.
  * Checks if user data is present in localStorage to determine the logged-in status.
  * Calls `navLogged` to display the navigation bar for logged-in users or `navNotLogged` for guests.
- * Independently calls `navSearchDisplay` to handle the search functionality in the navigation bar.
- * Assumes the presence of a `#nav-container` element in the DOM to host the navigation bar.
  *
  * @example
  * // To be called on page load or when the navigation bar needs to be updated
@@ -23,6 +20,4 @@ export const navDisplay = () => {
   } else {
     navNotLogged(navContainer);
   }
-
-  navSearchDisplay();
 };
